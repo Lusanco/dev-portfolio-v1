501af7b4e788462f8d8b66821dbfbda1696c3f58
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-12-31 01:10:36 -0400

    wip - working deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  outDir: "./docs",
+  site: "https://lusanco.github.io",
+  integrations: [tailwind()],
 });
